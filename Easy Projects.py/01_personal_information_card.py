cf7d296146bea0cfc97df0dc4758022374aae5eb
Proy <proythepro99@gmail.com>
2026-05-03 19:16:32 +0530

    Personal Information Card

---
+++ Easy Projects.py/01_personal_information_card.py
@@ -0,0 +1,16 @@
+#Personal Information Card
+
+print("Fill the details to get your PIC.")
+name=input("Enter your full name: ")
+age=int(input("Enter you age: "))
+height=float(input("Enter your height in meters: "))
+weight=float(input("Enter your weight in kilograms: "))
+city=input("Enter the name of your city: ")
+hobby=input("Enter your hobby: ")
+
+print(f"My Name is: {name}")
+print(f"I am {age} years old")
+print(f"I am {height} meters tall")
+print(f"My weight is {weight} kilograms")
+print(f"I live in {city}")
+print(f"My hobby is {hobby}")
