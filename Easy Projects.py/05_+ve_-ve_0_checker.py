d747b28d89b80193833cf3bcef6c02452e9b7890
Proy <proythepro99@gmail.com>
2026-05-04 19:30:47 +0530

    Positive Negetive and Zero checker

---
+++ Easy Projects.py/05_+ve_-ve_0_checker.py
@@ -0,0 +1,7 @@
+number=float(input("Enter your number:"))
+if number>0:
+    print(f"{number} is positive")
+elif number<0:
+    print(f"{number} is negetive")
+else:
+    print("The entered number is zero")
