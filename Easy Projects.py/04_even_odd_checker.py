62019c5eb69ea61889a317423591f9a0a65e02a0
proythepro99-hub <proythepro99@gmail.com>
2026-05-04 19:15:11 +0530

    Refactor even/odd checker logic
    Simplify even/odd check by removing zero case.

--- Easy Projects.py/04_even_odd_checker.py
+++ Easy Projects.py/04_even_odd_checker.py
@@ -1,7 +1,5 @@
 number=int(input("Enter a number: "))
-if number==0:
-    print(f"{number} is neither odd nor even")
-elif number%2==0:
+if number%2==0:
     print(f"{number} is an even number")
 else:
     print(f"{number} is an odd number")
