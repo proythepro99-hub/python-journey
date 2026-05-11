04ebefc0809fc3e4a7e845c80b7d8b85723eaf6a
Proy <proythepro99@gmail.com>
2026-05-11 21:12:21 +0530

    Password Strength Checker

--- Mediocre Projects.py/02_password_strength_checker.py
+++ Mediocre Projects.py/02_password_strength_checker.py
@@ -1,8 +1,15 @@
-password=input("Enter password: ")
-up=any(i.isupper()for i in password)
-low=any(i.islower()for i in password)
-num=any(i.isdigit()for i in password)
+password=input("Enter your password: ")
+up=False
+low=False
+num=False
+for ch in password:
+    if ch.isupper():
+        up=True
+    if ch.islower():
+        low=True
+    if ch.isdigit():
+        num=True
 if len(password)>8 and up and low and num:
-    print("Password is strong")
+    print(f"\"{password}\" is a strong password, it meets all the criterias")
 else:
-    print("Password is weak")
+    print(f"{password} is a weak passsword, it doesn't meet all the criterias")
